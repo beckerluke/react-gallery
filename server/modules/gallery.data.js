a94d0d701631d02a8c1ab0923b4fcfc3ce836cf4
Luke Becker <beckerluke1@gmail.com>
2019-08-23 15:57:18 -0500

    Added another photo and established initial setup packages

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -2,6 +2,7 @@ const galleryItems = [
     { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
     { id: 2, path: 'images/grizzly-bear.jpg', description: 'Photo of a grizzly bear roaring.', likes: 0 },
     { id: 3, path: 'images/mountain-lion.jpg', description: 'Photo of a mountain lion pouncing.', likes: 0 },
+    { id: 4, path: 'images/mahomes.jpg', description: 'Photo of Patrick Mahomes launching a beauty.', likes: 0 },
 ];
 
 module.exports = galleryItems;
