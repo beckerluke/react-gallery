25eb86e47b2ef16cacae531f2fb866f52c96824a
Luke Becker <beckerluke1@gmail.com>
2019-08-23 15:48:42 -0500

    Added images to images folder and modified gallery.data.js

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,7 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 2, path: 'images/grizzly-bear.jpg', description: 'Photo of a grizzly bear roaring.', likes: 0 },
+    { id: 3, path: 'images/mountain-lion.jpg', description: 'Photo of a mountain lion pouncing.', likes: 0 },
 ];
 
 module.exports = galleryItems;
