90b5ffb1c62d359120ee512beb5655a6d06da196
Luke Becker <beckerluke1@gmail.com>
2019-08-23 16:18:03 -0500

    Added basic GalleryList and GalleryItem components. Imported GalleryList into App.js

---
+++ src/components/GalleryList/GalleryList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class GalleryList extends Component {
+  render() {
+    return (
+        <img src="images/goat_small.jpg"/>
+    );
+  }
+}
+
+export default GalleryList;
