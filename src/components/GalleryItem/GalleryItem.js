ecf0e038b23961c0a5c2929ce9fce16b61b2d47e
Luke Becker <beckerluke1@gmail.com>
2019-08-23 20:54:01 -0500

    Got description to display once image is clicked

--- src/components/GalleryItem/GalleryItem.js
+++ src/components/GalleryItem/GalleryItem.js
@@ -2,24 +2,32 @@ import React, { Component } from 'react';
 // import axions from 'axions';
 
 class GalleryItem extends Component {
-    // revealDescriptionHandler = (event) => {
-        
-    // }
+    state = {
+        imageClicked: false,
+    }
+
+
+    revealDescriptionHandler(event) {
+        this.setState({
+            imageClicked: true
+        });
+        console.log(this.state.imageClicked); 
+    }
 
   render() {
+      let galleryImage = <img src={this.props.itemData.path} alt={this.props.itemData.description}/>
+      if (this.state.imageClicked) {
+          galleryImage = <div>{this.props.itemData.description}</div>
+      }
     return (
-
         <li>
-            <img 
-            src={this.props.itemData.path} 
-            alt={this.props.itemData.description} 
-            onClick={this.revealDescriptionHandler}    
-            />
-            {/* <button
-                    
-            >
-                Delete
-            </button> */}
+            <button onClick={(event) => this.revealDescriptionHandler(event)}>
+                {/* <img 
+                src={this.props.itemData.path} 
+                alt={this.props.itemData.description} 
+                /> */}
+                {galleryImage}
+            </button>
         </li> 
     );
   }
